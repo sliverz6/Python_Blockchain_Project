5b129fd9f68e34e344b3cbff7d71faf05820521e
Hotteok Mind <52848569+sliverz6@users.noreply.github.com>
2021-04-13 18:59:18 +0900

    verify_chian() 함수 리팩토링

--- blockchain.py
+++ blockchain.py
@@ -40,26 +40,38 @@ def print_blockchain_elements():
     for block in blockchain:
         print("Outputting Block")
         print(block)
+    else:
+        print("-" * 20)
 
 
 def verify_chain():
     """ 블록체인을 검사하여 유효하면 True를, 그렇지 않으면 False를 반환한다. """
-    block_index = 0
+    # block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        elif block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index += 1
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1
+    #         continue
+    #     elif block[0] == blockchain[block_index - 1]:
+    #         is_valid = True
+    #     else:
+    #         is_valid = False
+    #         break
+    #     block_index += 1
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("Please choose")
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
@@ -75,12 +87,15 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == "q":
-        break
+        waiting_for_input = False
     else:
-        print("Input was invalid, please pick a vlue from the list!")
+        print("Input was invalid, please pick a value from the list!")
     if not verify_chain():
+        print_blockchain_elements()
         print("Invalid blockchain!")
         break
+else:
+    print("User left!")
 
 
 print("Done!")
