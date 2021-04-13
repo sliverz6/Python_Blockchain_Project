e296f8bd17d66503c062ce2d7a0b35373607cb9f
Hotteok Mind <52848569+sliverz6@users.noreply.github.com>
2021-04-13 09:43:23 +0900

    사용자 입력 메뉴 개선
    if-else문을 사용해서 입력 가지수를 늘림.

--- blockchain.py
+++ blockchain.py
@@ -9,32 +9,47 @@ def get_last_blockchain_value():
 
 def add_value(transaction_amount, last_transaction=[1]):
     """ 
-    블록체인 리스트에 마지막 통화와 함께 새로운 통화를 추가한다.
+    블록체인 리스트에 마지막 트랜잭션 함께 새로운 통화를 추가한다.
 
     인수: 
-        :transaction_amount: 추가될 통화량
-        :last_transaction: 마지막으로 추가된 통화 (기본값 [1])
+        :transaction_amount: 추가될 트랜잭션량
+        :last_transaction: 마지막으로 추가된 트랜잭션 (기본값 [1])
 
     """
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
-    """ 사용자에게 새로운 통화량을 입력받아 반환한다(부동소수점 숫자). """
+def get_transaction_value():
+    """ 사용자에게 새로운 트랜잭션량을 입력받아 반환한다(부동소수점 숫자). """
     user_input = float(input("Your transaction amount please: "))
     return user_input
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    """ 사용자에게 메뉴 선택을 입력받아 반환한다. """
+    user_input = input("Your choice: ")
+    return user_input
 
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
 
-    # 블록체인 리스트를 콘솔에 출력한다
+def print_blockchain_elements():
+    """ 현재 블록체인 리스트를 콘솔에 출력한다. """
     for block in blockchain:
         print("Outputting Block")
         print(block)
+
+
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+while True:
+    print("Please choose")
+    print("1: Add a new transaction value")
+    print("2: Output the blockchain blocks")
+    user_choice = get_user_choice()
+    if user_choice == "1":
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
 
 print("Done!")
